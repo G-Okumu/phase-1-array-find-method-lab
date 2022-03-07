a9fee36d0ed64cdb93e04dd2d489d77180afd304
George-Okumu <gokumu12@gmail.com>
2022-03-07 07:28:38 +0300

    assigment complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+// function superbowlWin(results) {
+//     let result = results.find(results.result === "W");
+//     if (result == true) {
+//         return year;
+//     } else {
+//         return undefined;
+//     }
+// }
+superbowlWin = (record) => {
+    const result = record.find(record => record.result === "W");
+    return !!result ? result.year : undefined;
+}
